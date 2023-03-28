86374e07546881867093c373080c9d76a9759622
anita-tsai <anita83110@gmail.com>
2023-03-28 18:37:59 +0800

    Updates

---
+++ precache-manifest.64bbc11a6c0fcc07c38295ecc4da8c2e.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = [
+  {
+    "revision": "2760767d14e67fef1b80",
+    "url": "/doremi_demo/static/css/main.2b255828.chunk.css"
+  },
+  {
+    "revision": "2760767d14e67fef1b80",
+    "url": "/doremi_demo/static/js/main.2760767d.chunk.js"
+  },
+  {
+    "revision": "0fae0e8c0ea5a51eb9db",
+    "url": "/doremi_demo/static/js/1.0fae0e8c.chunk.js"
+  },
+  {
+    "revision": "2be3cb1ba22d4ab757f0",
+    "url": "/doremi_demo/static/js/2.2be3cb1b.chunk.js"
+  },
+  {
+    "revision": "4564a487665b6e9d0fff",
+    "url": "/doremi_demo/static/js/runtime~main.4564a487.js"
+  },
+  {
+    "revision": "8195380559a11a853ebccd274bbe9f91",
+    "url": "/doremi_demo/static/media/createRoom.81953805.gif"
+  },
+  {
+    "revision": "7763444ee7b698701b411dfffd41c2ad",
+    "url": "/doremi_demo/static/media/deleteRoom.7763444e.gif"
+  },
+  {
+    "revision": "b820cc0ba82edee9f0eb8a9f9888432c",
+    "url": "/doremi_demo/static/media/editRoom.b820cc0b.gif"
+  },
+  {
+    "revision": "deb49a22d0d4da80e67b6fa5c02a0a06",
+    "url": "/doremi_demo/static/media/facebookLogin.deb49a22.gif"
+  },
+  {
+    "revision": "a84d9b0bd6f992cf5088394e52f9df50",
+    "url": "/doremi_demo/static/media/googleLogin.a84d9b0b.gif"
+  },
+  {
+    "revision": "2046b0849e5289e07e9fda3753914825",
+    "url": "/doremi_demo/static/media/homepage.2046b084.png"
+  },
+  {
+    "revision": "08b982a1e6db620eeeea0d2b1db323e3",
+    "url": "/doremi_demo/static/media/interaction.08b982a1.gif"
+  },
+  {
+    "revision": "f0933cf8803af4cf2681acfa1f8f2e29",
+    "url": "/doremi_demo/static/media/textSearch.f0933cf8.gif"
+  },
+  {
+    "revision": "be259f41b2bcf4a328725af369323572",
+    "url": "/doremi_demo/static/media/categorySearch.be259f41.gif"
+  },
+  {
+    "revision": "3062016db2fc18c92e2f98c5e8839880",
+    "url": "/doremi_demo/static/media/tagSearch.3062016d.gif"
+  },
+  {
+    "revision": "14b6d5f6e3fb49e10a1f6e4a867523cb",
+    "url": "/doremi_demo/static/media/userPage.14b6d5f6.gif"
+  },
+  {
+    "revision": "f7260f17f93225f3c76a93dc1325bb42",
+    "url": "/doremi_demo/static/media/blacklist.f7260f17.gif"
+  },
+  {
+    "revision": "e78aff2dc8ae5df3dfb4940810b716e7",
+    "url": "/doremi_demo/static/media/submitBlacklist.e78aff2d.gif"
+  },
+  {
+    "revision": "f4bf9e64fcf14f318653ae25ad2622fa",
+    "url": "/doremi_demo/index.html"
+  }
+];
